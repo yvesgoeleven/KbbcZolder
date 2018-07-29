5d3f546bc27de5618fa4b5cc5f2a2632ce8ae857
Yves Goeleven <yves@goeleven.com>
2018-07-29 17:02:31 +0200

    publish support

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll --theme Zolder --port 5002
+dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll serve --theme Zolder --port 5002
