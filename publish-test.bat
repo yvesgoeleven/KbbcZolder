28fadcfff632b580061928b724dcf1de2b53695f
Yves Goeleven <yves@goeleven.com>
2019-01-23 22:05:56 +0100

    publish profile for production

---
+++ publish-test.bat
@@ -0,0 +1,1 @@
+dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll publish --theme Zolder --url "https://test.basketzolder.be/" --connectionStringName dish-publish-zolder-test
--- publish.bat
+++
@@ -1,1 +0,0 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\netcoreapp2.0\dish.dll publish --theme Zolder --url "https://www.basketzolder.be/" --connectionStringName dish-publish-zolder
