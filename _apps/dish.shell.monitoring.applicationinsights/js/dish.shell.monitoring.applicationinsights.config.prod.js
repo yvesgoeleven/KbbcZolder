49236c0f8e907a1681b736e74fcc095a4642b01e
Yves Goeleven <yves@goeleven.com>
2023-10-31 23:19:06 +0100

    fix correlation

--- _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.prod.js
+++ _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.prod.js
@@ -1,7 +1,8 @@
 const appinsightsConfig = {
     instrumentationKey: "fd161f76-2256-4df2-a707-36ae6140ea51",
 	role: "kbbczolder.be",
-	roleInstance: "www.kbbczolder.be"
+	roleInstance: "www.kbbczolder.be",
+	correlationHeaderDomains: ['*.clubmanagement.io', '*.azurewebsites.net']
 };
 
 export { appinsightsConfig }
--- _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.test.js
+++ _apps/dish.shell.monitoring.applicationinsights/js/dish.shell.monitoring.applicationinsights.config.test.js
@@ -1,7 +1,8 @@
 const appinsightsConfig = {
     instrumentationKey: "61b1b375-fed5-4cb0-911d-e3d8300dc562",
 	role: "kbbczolder.be",
-	roleInstance: "test.kbbczolder.be"
+	roleInstance: "test.kbbczolder.be",
+	correlationHeaderDomains: ['*.clubmanagement.io', '*.azurewebsites.net']
 };
 
 export { appinsightsConfig }
