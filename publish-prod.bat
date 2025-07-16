2ec1625021657338a2d99fb7803253e11d48bcfc
yvesgoeleven <yves@goeleven.com>
2025-07-16 13:13:22 +0200

    fix file name & grid layou

--- publish-prod.bat
+++ publish-prod.bat
@@ -1,1 +1,1 @@
-dotnet C:\github\dish\sitegenerator\src\Dish.SiteGenerator\bin\Debug\net6.0\dish.dll publish --theme Zolder --url "https://www.basketzolder.be/" --connectionStringName dish-publish-zolder-prod
+dish publish --theme Zolder --url "https://www.basketzolder.be/" --connectionStringName dish-publish-zolder-prod
